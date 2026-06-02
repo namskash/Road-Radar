0111125a096342e9f0a11f0ff7b2240f724a308d
Naman Kashyap <namskash@gmail.com>
2026-06-02 11:03:24 +0530

    chore: Update scripts to mirror language concentration accurately

--- scripts/NK.kt
+++ scripts/NK.kt
@@ -1,3 +1,13 @@
 fun main() {
   println("Hello from Road-Radar!")
+  println("I use nvim, btw")
+  println("And Arch Linux... Ok, I use EndeavourOS to be exact, but it's basically Arch! Thank you!")
+
+  // I will countdown from 10:
+  for (i in 10 downTo 1) {
+    println(i)
+    Thread.sleep(1000)
+  }
+
+  println("Check out a demo at https://www.youtube.com/watch?v=xEX3r39b6Z8!") // https://www.youtube.com/watch?v=xEX3r39b6Z8
 }
